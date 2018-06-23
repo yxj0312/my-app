6bd894879eae04d89e2238821de88253fc940ca5
yxj0312 <yanxiajun@hotmail.com>
2018-06-23 22:44:58 +0200

    Ep 16 Webpack and vue-cli and Git initialize Repo

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <message>Hello There</message>
+    <message>Hello There Again</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+  components: { Message },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [],
+
+        components: {},
+
+        data() {
+            return {
+                
+            }
+        },
+
+        computed: {
+            
+        },
+
+        methods: {
+            
+        }
+    }
+</script>
+
+<style>
+    .box { background: #e3e3e3; padding: 10px; border: 1px solid #c5c5c5; margin-bottom: 1em;}
+</style>
