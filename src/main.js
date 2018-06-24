418d7f59d2c378e74f97ac8a05ce05af1d21d5f5
yxj0312 <yanxiajun@hotmail.com>
2018-06-24 22:47:01 +0200

    Ep 17 Shared State 101 and use the newest vue-cli to create a new project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
-import Vue from 'vue'
+// import Vue from 'vue'
+import Vue from 'vue/dist/vue.js'
 import App from './App.vue'
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+let store = {
+  user: {
+      name:'John Doe'
+    }
+}
+new Vue({
+  el: '#app1',
+  
+  data: {
+    foo: 'bar',
+    shared: store
+  }
+})
+
+new Vue({
+  el: '#app2',
+  
+  data: {
+    foo: 'otherbar',
+    shared: store
+  }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -71,7 +71,8 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js',
+      // 'vue': 'vue/dist/vue.js'
     },
     extensions: ['*', '.js', '.vue', '.json']
   },
